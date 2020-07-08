fbf67a6df5d98b54e70ecec7131fe649c0fd41f5
cAttte <26514199+cAttte@users.noreply.github.com>
2020-07-08 14:20:24 -0300

    remove FetchError from module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-module.exports.SolveMediaAPIError = require("./SolveMediaAPIError")
-module.exports.AuthorizationError = require("./AuthorizationError")
-module.exports.FetchError = require("node-fetch").FetchError
 module.exports.SolveMediaClient = require("./SolveMediaClient")
 module.exports.Challenge = require("./Challenge")
+module.exports.SolveMediaAPIError = require("./SolveMediaAPIError")
+module.exports.AuthorizationError = require("./AuthorizationError")
