f5751f3e454bad5700702444f2336cf73e82db7c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-07-08 01:46:50 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const SolveMediaAPIError = require("./SolveMediaAPIError")
 module.exports.SolveMediaAPIError = require("./SolveMediaAPIError")
 module.exports.AuthorizationError = require("./AuthorizationError")
 module.exports.FetchError = require("node-fetch").FetchError
